233346ad0058a277727f138e1ab48bf300666e1a
Mick Burgess <mburgess@reliaslearning.com>
2018-05-05 00:25:32 -0400

    added more js, not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,7 +16,7 @@ var remainingLetters = nextWord.length;
 
 // Sets the number of total guesses you have
 var guessTotal = nextWord.length + 5;
-document.querySelector(".guess-remaining").innerHTML = guessTotal;
+document.querySelector("#guess-remaining").innerHTML = guessTotal;
 // Sets up an array of underlines equal to the length of nextWord
 var answerArray = [];
 for (i = 0; i < nextWord.length; i++) {
@@ -29,35 +29,42 @@ for (i = 0; i < nextWord.length; i++) {
 
 
   // Will loop as long as there are letters unguessed in nextWord
-  if (remainingLetters > 0 && guessLetters.length < guessTotal) {
   
-  function keyPress(logAnswer) {
+function keyPress() {
     // Determines which key was pressed
     document.onkeyup = function (event) {
       var userGuess = event.key.toUpperCase();
+      
       // Checks if guess is in nextWord and if it is then add it to our answer and subtract from the remaining letters to guess
       for (var j = 0; j < nextWord.length; j++) {
         if (nextWord[j] === userGuess) {
-          answerString[j].push(userGuess);
+          answerArray[j] = userGuess;
           remainingLetters--;
-        }
-        else {
-          // Add incorrect guesses to an array
-          guessLetters.push(userGuess);
-        }
+        }   
       }
-      console.log(userGuess);
       guessTotal--;
-      console.log(guessTotal); 
+      guessLetters.push(userGuess);
     }
-  }
+  
   console.log(remainingLetters);
   console.log(answerArray);
   console.log(guessLetters);
+
+ /* if (remainingLetters < 1) {
+    document.getElementById("current-word").innerHTML = answerString;
+    wins++
+    document.querySelector("#win-total").innerHTML = wins;
+  }
+
+  else if (guessTotal < 1) {
+    var wrapper = document.querySelector(".wrapper");
+    wrapper.setAttribute("src", "../images/diablo.jpg");
+  }
+
+  else {
+    document.getElementById("current-word").innerHTML = answerString;
+    document.querySelector("#guess-letters").innerHTML = guessLetters.join(" ");
+  }*/
 }
 
 keyPress();
-// Will use to make first letter uppercase
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
