9972053c924b4c69b01c3f0fede70796fdb2200a
Mick Burgess <mburgess@reliaslearning.com>
2018-05-09 13:33:58 -0400

    fixed previous commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,70 @@
+$(document).ready(function() {
+
+
+
 // GLOBAL VARIABLES
 
 // Array of words to be used for hangman
 var wordBank = ["DIABLO", "NEPHALEM", "BARBARIAN", "AMAZON", "ANDARIEL", "MEPHISTO", "CRUSADER", "PALADIN", "ASSASSIN", "DRUID", "CAIN", "MITIGATION", "HORADRIM"]
 
+// Get a random word from the wordBank array
+var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+console.log(currentWord);
+
+// Sets up an array of underlines equal to the length of currentWord
+var answerArray = [];
+for (i = 0; i < currentWord.length; i++) {
+  answerArray[i] = "_";
+}
+
+// Combine all blanks in answerArray with a space in between to a string
+answerString = answerArray.join(" ");
+document.getElementById("current-word").innerText = answerString;
+
+// Sets the number of total guesses you have
+var guessTotal = currentWord.length + 5;
+document.querySelector("#guess-remaining").innerText = guessTotal;
+
+// Used at before game begins
 var messageBlock = "PRESS ANY KEY TO GET STARTED!";
 document.getElementById("start").innerText = messageBlock;
 
+// initialize game
+function initializeGame () {
+  document.getElementById("start").innerText = messageBlock;
+  
+}
+
+// Win tally
 var wins = 0;
 document.getElementById("win-total").innerText = wins;
 
 // Holds the letters already guessed
 var guessLetterArray = [];
 
-// Get a random word from the wordBank array
-var nextWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-console.log(nextWord);
-
 // The number of letters remaining that are unguessed
-var remainingLetters = nextWord.length;
+// var remainingLetters = currentWord.length;
 
-// Sets the number of total guesses you have
-var guessTotal = nextWord.length + 5;
-document.querySelector("#guess-remaining").innerText = guessTotal;
-// Sets up an array of underlines equal to the length of nextWord
-var answerArray = [];
-for (i = 0; i < nextWord.length; i++) {
-  answerArray[i] = "_";
-}
-
-  // Combine all blanks in answerArray with a space in between to a string
-  answerString = answerArray.join(" ");
-  document.getElementById("current-word").innerText = answerString;
-
-
-  // Will loop as long as there are letters unguessed in nextWord
+  // Will loop as long as there are letters unguessed in currentWord
   
 function keyPress() {
-    // Determines which key was pressed
-    document.onkeyup = function (event) {
-      var userGuess = event.key.toUpperCase();
-      document.getElementById("start").innerText = "";
-      for (var i = 0; i < guessLetterArray.length; i++) { if (guessLetterArray[i] !== userGuess) {
-        guessLetterArray.push(userGuess);
-        guessLetter = guessLetterArray.join(" ");
-      }
-    //  console.log(userGuess);
-      // console.log(nextWord);
-      var nextSplit = nextWord.split("");
+  // Determines which key was pressed
+  document.onkeyup = function (event) {
+    var userGuess = event.key.toUpperCase();
+    document.getElementById("start").innerText = " ";
+    /*for (var i = 0; i < guessLetterArray.length; i++) { if (guessLetterArray[i] !== userGuess) {
+      guessLetterArray.push(userGuess);
+      guessLetter = guessLetterArray.join(" ");
+    }*/
+      var nextSplit = currentWord.split("");
       // console.log(nextSplit);
-      // console.log("length of string", nextWord.length);
-      // Checks if guess is in nextWord and if it is then add it to our answer and subtract from the remaining letters to guess
-      for (var j = 0; j < nextWord.length; j++) {
+      // console.log("length of string", currentWord.length);
+      // Checks if guess is in currentWord and if it is then add it to our answer and subtract from the remaining letters to guess
+      for (var j = 0; j < currentWord.length; j++) {
         if (nextSplit[j] === userGuess) {
           answerArray[j] = userGuess;
-          remainingLetters--
-          console.log("remaining", remainingLetters);
+          // remainingLetters--;
+          // console.log("remaining", remainingLetters);
           // console.log(answerArray);
           answerString = answerArray.join(" ");
           document.getElementById("current-word").innerHTML = answerString;
@@ -73,38 +83,25 @@ function keyPress() {
     }
   
     function winChecker () {
-      var answerSplit = nextWord.split("");
+      var answerSplit = currentWord.split("");
       console.log("winChecker", answerSplit);
       console.log("answer", answerArray);
       var counter = 0;
-      for (var k =0; k < nextWord.length; k++) {
-       
+      for (var k =0; k < currentWord.length; k++) {
         // console.log( answerSplit[k]);
         if (answerSplit[k] === answerArray[k]) {
           counter++;
         }  
       }
-      if (counter == nextWord.length) {
+      if (counter == currentWord.length) {
+        wins++;
+        document.getElementById("win-total").innerText = wins;
+
         console.log("success");
       }
     }
-  
-
- /* if (remainingLetters < 1) {
-    document.getElementById("current-word").innerHTML = answerString;
-    wins++
-    document.querySelector("#win-total").innerHTML = wins;
-  }
-
-  else if (guessTotal < 1) {
-    var wrapper = document.querySelector(".wrapper");
-    wrapper.setAttribute("src", "../images/diablo.jpg");
-  }
-
-  else {
-    document.getElementById("current-word").innerHTML = answerString;
-    document.querySelector("#guess-letters").innerHTML = guessLetters.join(" ");
-  }*/
 }
 
 keyPress();
+
+})
