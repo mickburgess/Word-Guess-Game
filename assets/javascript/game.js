a049e078f87dec7dd1243a9c2e1372179c512e09
Mick Burgess <mburgess@reliaslearning.com>
2018-05-09 21:49:06 -0400

    add logic to not distract guesses if same key pressed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,9 +11,6 @@ var wordBank = ["DIABLO", "NEPHALEM", "BARBARIAN", "AMAZON", "ANDARIEL", "MEPHIS
 var wins = 0;
 document.getElementById("win-total").innerText = wins;
 
-// Holds the letters already guessed
-var guessLetterArray = [];
-
 // The number of letters remaining that are unguessed
 // var remainingLetters = currentWord.length;
 
@@ -24,51 +21,63 @@ function setCurrentWord() {
 
   // Get a random word from the wordBank array
   var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-  console.log(currentWord);
+  // console.log(currentWord);
 
   // Sets up an array of underlines equal to the length of currentWord
   var answerArray = [];
   for (i = 0; i < currentWord.length; i++) {
   answerArray[i] = "_";
   }
+
   // Combine all blanks in answerArray with a space in between to a string
   answerString = answerArray.join(" ");
   document.getElementById("current-word").innerText = answerString;
+  
   // Sets the number of total guesses you have
   var guessTotal = currentWord.length + 5;
   document.querySelector("#guess-remaining").innerText = guessTotal;
+
+  // Holds the letters already guessed
+  var guessLetterArray = [];
+  console.log("guess", guessLetterArray);
 
   function keyPress() {
     // Determines which key was pressed
     document.onkeyup = function (event) {
       var userGuess = event.key.toUpperCase();
       document.getElementById("start").innerText = " ";
-      /*for (var i = 0; i < guessLetterArray.length; i++) { if (guessLetterArray[i] !== userGuess) {
+      if (guessLetterArray.includes(userGuess)) {
+        duplicateResponse();
+      }
+      else {
         guessLetterArray.push(userGuess);
+        console.log("guess2", guessLetterArray);
         guessLetter = guessLetterArray.join(" ");
-      }*/
-        var nextSplit = currentWord.split("");
-        // console.log(nextSplit);
-        // console.log("length of string", currentWord.length);
-        // Checks if guess is in currentWord and if it is then add it to our answer and subtract from the remaining letters to guess
+        console.log("guess3", guessLetter);
+        document.getElementById("guess-letters").innerText = guessLetter;
+        guessTotal--;
+        document.getElementById("guess-remaining").innerText = guessTotal;
+        console.log(guessTotal);
+      }
+      
+      function duplicateResponse() {
+        document.getElementById("start").innerText = "Already guessed. Please try a different letter."
+      }
+
+        var currentSplit = currentWord.split("");
+
+        // Checks if guess is in currentWord and if it is then add it to our answer
         for (var j = 0; j < currentWord.length; j++) {
-          if (nextSplit[j] === userGuess) {
+          if (currentSplit[j] === userGuess) {
             answerArray[j] = userGuess;
-            // remainingLetters--;
             // console.log("remaining", remainingLetters);
             // console.log(answerArray);
             answerString = answerArray.join(" ");
             document.getElementById("current-word").innerHTML = answerString;
-            guessLetterArray.push(userGuess);
-            guessLetter = guessLetterArray.join(" ");
-            //document.getElementById("#guess-letters").innerHTML = guessLetter;
-            console.log("guess", guessLetter);
+           
+           
           }   
         }
-        guessTotal--;
-        document.getElementById("guess-remaining").innerText = guessTotal;
-        console.log(guessTotal);
-
         
         winChecker()
       }
